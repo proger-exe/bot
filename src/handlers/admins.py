f49f475675d379f4f21fff79f79e6d8db933e337
Paradoxant <70023601+proger-exe@users.noreply.github.com>
2024-08-20 13:45:20 +0400

    Changed texts...:D

--- src/handlers/admins.py
+++ src/handlers/admins.py
@@ -2,15 +2,14 @@ from aiogram import types
 
 from aiogram.utils import exceptions
 
+
 async def ban_moder(msg: types.Message):
     try:
         user = await msg.bot.get_chat_member(msg.chat.id, msg.reply_to_message.from_id)
 
         if user.is_chat_admin():
-            await msg.reply("Че, ебанулся? Я админа не смогу забанить.")
-
-            return
-
+            return await msg.reply("Забанить админа? Хорошая идея, но пожалуй, вы справитесь с этим сами.")
+            
         await msg.bot.kick_chat_member(chat_id=msg.chat.id, user_id=msg.reply_to_message.from_id)
 
         await msg.answer(f"Пользователь {msg.reply_to_message.from_user.full_name} был забанен!")
@@ -18,22 +17,21 @@ async def ban_moder(msg: types.Message):
     except exceptions.BadRequest:
         await msg.answer("Пользователя нет или что-то не так..")
 
+
 async def mute_moder(msg: types.Message):
     try:
         user = await msg.bot.get_chat_member(msg.chat.id, msg.reply_to_message.from_id)
 
         if user.is_chat_admin():
-            await msg.answer("Щас бы админа мутить, м...")
-
-            return 
+            return  await msg.answer("Замутить админа? Хорошая идея, но пожалуй, вы справитесь с этим сами..")
         
         await msg.bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_id, types.ChatPermissions(False))
 
         await msg.answer(f"Пользователь {msg.reply_to_message.from_user.full_name} был замучен!")
-
     
     except exceptions.BadRequest:
         await msg.answer("Пользователя нет или что-то не так..")   
+
 
 async def unban_moder(msg: types.Message):
     try:
@@ -43,6 +41,7 @@ async def unban_moder(msg: types.Message):
     
     except exceptions.BadRequest:
         await msg.answer("Пользователя нет или что-то не так..")
+
 
 async def unmute_moder(msg: types.Message):
 
