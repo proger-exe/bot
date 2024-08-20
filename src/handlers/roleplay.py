0a07dc1fe4eef8b8f022f598e11dd24b25915655
Paradoxant <70023601+proger-exe@users.noreply.github.com>
2024-08-20 13:46:42 +0400

    Update roleplay.py

--- src/handlers/roleplay.py
+++ src/handlers/roleplay.py
@@ -11,7 +11,7 @@ async def kill_rp(msg: types.Message):
     await msg.answer(messager.get_html("убил(а)", msg))
 
 async def fuck_rp(msg: types.Message):
-    await msg.answer(messager.get_html("выебал(а)", msg))
+    await msg.answer(messager.get_html("принудил(а) к интиму", msg))
 
 async def kiss_rp(msg: types.Message):
     await msg.answer(messager.get_html("поцеловал(а)", msg))
